73d00709e000c74f5fc4cce1a7a5c24d55d65548
gytis <Gytis.kymantas@yahoo.com>
2022-01-21 00:39:31 +0200

    Added dependencies

---
+++ src/homepage.component.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const HomePage = () => {
+   return( <div className="homepage">
+        <div className="directory-menu">
+            <div classnName="menu-item">
+                <div className="content">
+                    <h1 className="title">HATS</h1>
+                    <span className="subtitle">ShopNow</span>
+                </div>
+            </div>
+            <div classnName="menu-item">
+                <div className="content">
+                    <h1 className="title">JACKETS</h1>
+                    <span className="subtitle">ShopNow</span>
+                </div>
+            </div>
+            <div classnName="menu-item">
+                <div className="content">
+                    <h1 className="title">SNEAKERS</h1>
+                    <span className="subtitle">ShopNow</span>
+                </div>
+            </div>
+            <div classnName="menu-item">
+                <div className="content">
+                    <h1 className="title">WOMEN</h1>
+                    <span className="subtitle">ShopNow</span>
+                </div>
+            </div>
+            <div classnName="menu-item">
+                <div className="content">
+                    <h1 className="title">MEN</h1>
+                    <span className="subtitle">ShopNow</span>
+                </div>
+            </div>
+        </div>
+    </div>)
+}
+export default HomePage;
